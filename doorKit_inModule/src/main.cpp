25c750691a539eaf416cd86154f65fc51b608d1e
drinktoomuchsax <drinktoomuchsax@qq.com>
2023-07-17 10:40:35 +0800

    fix inMoudle html open lock state both survive bug

--- doorKit_inModule/src/main.cpp
+++ doorKit_inModule/src/main.cpp
@@ -221,24 +221,32 @@ void loop()
             {
               Serial.println("GPIO 26 on");
               openDoorState = "on";
+              lockREDState = "off";
               digitalWrite(openDoor, HIGH);
+              digitalWrite(lockRED, LOW);
             }
             else if (header.indexOf("GET /openDoorState/off") >= 0)
             {
               Serial.println("GPIO 26 off");
               openDoorState = "off";
+              lockREDState = "off";
               digitalWrite(openDoor, LOW);
+              digitalWrite(lockRED, LOW);
             }
             else if (header.indexOf("GET /lockREDState/on") >= 0)
             {
               Serial.println("GPIO 27 on");
+              openDoorState = "off";
               lockREDState = "on";
+              digitalWrite(openDoor, LOW);
               digitalWrite(lockRED, HIGH);
             }
             else if (header.indexOf("GET /lockREDState/off") >= 0)
             {
               Serial.println("GPIO 27 off");
+              openDoorState = "off";
               lockREDState = "off";
+              digitalWrite(openDoor, LOW);
               digitalWrite(lockRED, LOW);
             }
 
